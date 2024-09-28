0cd27f2c5c370b2b909130b05cf02b408f6550aa
Sabarehmani <sabarehmani287@gmail.com>
2024-09-28 10:54:38 -0700

    loops

--- while-loops.py
+++ while-loops.py
@@ -4,6 +4,14 @@ while count <= 5 :
     count +=1
 
 i = 1
-while i <= 1000000:
+while i <= 1000:
     print("sabarazzaq", i)
     i+=1
+
+
+i = 1
+while i <= 5:
+    print(i)
+    i+=1
+
+    print("loop ended")
