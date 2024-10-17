ef850b96145fb610e7189aec928eb4ca7599cf54
Sabarehmani <sabarehmani287@gmail.com>
2024-10-17 09:19:08 -0700

    continue

--- break&continue.py
+++ break&continue.py
@@ -4,3 +4,21 @@ while i <= 5:
     if(i==  3):
         break
     i += 1
+
+    print("end of loop")
+
+
+
+nums = (1, 3, 5, 8, 23, 45, 67, 89, 100)
+
+x = 45
+
+i = 0
+while i < len(nums):
+    if(nums[i] == x):
+        print("FOUND at idx", i)
+        break
+    else:
+        print("finding....")
+    i += 1
+    print("end of loop")
